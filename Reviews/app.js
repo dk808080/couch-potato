50c0a3e7c69c0663485ec4fada39d52f06b86556
vartika1203 <54345621+vartika1203@users.noreply.github.com>
2020-07-28 14:43:57 +0530

    Update app.js

--- Reviews/app.js
+++ Reviews/app.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
 
 const homeStartingContent = "The reviews must be added here";
 const app = express();
@@ -38,21 +37,6 @@ app.post("/reviews", function(req, res){
 
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
-
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
